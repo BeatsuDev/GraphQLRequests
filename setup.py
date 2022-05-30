6ce9a11d84871a408ffaae7d74c5fae7a73544a1
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2022-05-30 11:53:18 +0200

    Added pypi setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+from gqlrequests import __version__
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gqlrequests",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["gqlrequests"],
+    install_requires=[],
+    license="MIT",
+    version=__version__,
+    description="A Python library for making GraphQL requests easier!",
+    python_requires=">=3.7.0",
+    author="BeatsuDev",
+    author_email="",
+    url="https://github.com/BeatsuDev/GraphQLRequests",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
