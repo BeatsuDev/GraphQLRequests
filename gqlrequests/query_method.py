be3638ef1f31b047447351f8e80dd226ac533818
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2023-01-07 06:19:12 +0100

    Small fixes to QueryMethod

--- gqlrequests/query_method.py
+++ gqlrequests/query_method.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Optional, Any, Dict, List
 
 from .query import Query
 
@@ -13,20 +13,24 @@ class QueryMethod(Query):
         self,
         method_name: str,
         dataclass_schema: DataclassType,
-        method_args: dict[str, Any],
-        **kwargs,
+        args: Optional[Dict[str, Any]] = None,
+        fields: Optional[List[str | Query | QueryMethod]] = None,
+        indents: int = 4,
     ):
-        super().__init__(**kwargs)
+        super().__init__(dataclass_schema, fields, indents)
         self.method_name = method_name
-        self.method_args = method_args
+        self.dataclass_schema = dataclass_schema
+        self.args = args or {}
 
     def __str__(self) -> str:
         argument_list = []
-        for key, value in self.method_args.items():
+        for key, value in self.args.items():
             if isinstance(value, str):
                 argument_list.append(f'{key}: "{value}"')
+            elif isinstance(value, bool):
+                argument_list.append(f"{key}: {str(value).lower()}")
             else:
                 argument_list.append(f"{key}: {value}")
 
         arguments = ", ".join(argument_list)
-        return f"{self.method_name}({arguments}) " + str(self.dataclass_schema)
+        return f"{self.method_name}({arguments}) " + super()._generate_query(self.indents)
