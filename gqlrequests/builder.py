fe6faf708a9d3063012b4b877e229a99e8700a81
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-08-27 14:01:02 +0200

    refac: use ruff instead of isort, flake8 and black

--- gqlrequests/builder.py
+++ gqlrequests/builder.py
@@ -1,21 +1,23 @@
-"""Contains abstract classes to be inherited by classes with types hints. 
+"""Contains abstract classes to be inherited by classes with types hints.
 These are then use to build GraphQL query strings in a Pythonic way."""
+
 from __future__ import annotations
+
 import abc
 import enum
 import typing
 
 
 class QueryBuilder(abc.ABC):
-    """An abstract class used to build GraphQL queries. 
-    
+    """An abstract class used to build GraphQL queries.
+
     This class should be inherited by a class with type hints and only
     supports single-leveled inheritance. This version of the QueryBuilder
     does not support reserved keywords as field names. To use reserved
     keywords, check the `StrippedUnderscoresQueryBuilder` class.
-    
+
     Example usage:
-        
+
         class EveryType(gqlrequests.QueryBuilder):
             id: int
             age: int
@@ -29,9 +31,10 @@ class QueryBuilder(abc.ABC):
         graphql_query_string = every_type.build()
 
     """
+
     SUPPORTED_TYPES = (int, float, str, bool)
 
-    _resolved_fields: dict[str, type | QueryBuilder ] | None = None
+    _resolved_fields: dict[str, type | QueryBuilder] | None = None
 
     indent_size: int = 4
     start_indents: int = 0
@@ -53,7 +56,9 @@ class QueryBuilder(abc.ABC):
 
             for field in passed_fields:
                 if field not in (self._resolved_fields or []):
-                    raise AttributeError(f"{field} is not a valid field for this builder.")
+                    raise AttributeError(
+                        f"{field} is not a valid field for this builder."
+                    )
 
         self.fields = passed_fields
         self.indent_size = options.pop("indent_size", self.indent_size)
@@ -61,8 +66,10 @@ class QueryBuilder(abc.ABC):
         self.func_name = options.pop("func_name", self.func_name)
 
         if options:
-            raise ValueError("The following options are not valid options for a" \
-                             "QueryBuilder: " + ", ".join(options.keys()))
+            raise ValueError(
+                "The following options are not valid options for a"
+                "QueryBuilder: " + ", ".join(options.keys())
+            )
 
     def build(self) -> str:
         """Generates a GraphQL query string based on the fields set in the
@@ -71,9 +78,9 @@ class QueryBuilder(abc.ABC):
             raise ValueError("No fields were selected for the query builder.")
         build_output = "{\n"
         build_output += self._generate_fields()
-        build_output += " "*self.start_indents + "}\n"
+        build_output += " " * self.start_indents + "}\n"
         return build_output
-    
+
     def _generate_fields(self) -> str:
         # Guards
         fields_to_generate: dict[str, type | QueryBuilder] = {}
@@ -84,7 +91,9 @@ class QueryBuilder(abc.ABC):
         if self.fields:
             for field in self.fields:
                 if field not in self._resolved_fields:
-                    raise AttributeError(f"{field} is not a valid field for this builder.")
+                    raise AttributeError(
+                        f"{field} is not a valid field for this builder."
+                    )
                 fields_to_generate[field] = self._resolved_fields[field]
         else:
             fields_to_generate = self._resolved_fields.copy()
@@ -98,11 +107,12 @@ class QueryBuilder(abc.ABC):
             #    name: str
             #    NestedType  # <---
             # }
-            if type(field_type) in (type, abc.ABCMeta) and issubclass(field_type, QueryBuilder): # type: ignore
+            if type(field_type) in (type, abc.ABCMeta) and issubclass(
+                field_type, QueryBuilder
+            ):  # type: ignore
                 FieldBuilder = field_type
-                field_builder = FieldBuilder( # type: ignore
-                    start_indents = len(whitespaces),
-                    indent_size=self.indent_size
+                field_builder = FieldBuilder(  # type: ignore
+                    start_indents=len(whitespaces), indent_size=self.indent_size
                 )
                 fields_string_output += whitespaces + f"{field} {field_builder.build()}"
 
@@ -116,9 +126,11 @@ class QueryBuilder(abc.ABC):
             # {
             #   name
             # }
-            elif self._resolved_fields[field] in self.SUPPORTED_TYPES or (type(field_type) == enum.EnumType):
+            elif self._resolved_fields[field] in self.SUPPORTED_TYPES or (
+                type(field_type) == enum.EnumType
+            ):
                 fields_string_output += whitespaces + field + "\n"
-            
+
             else:
                 raise ValueError(f"Could not build {field} of type {field_type}.")
 
