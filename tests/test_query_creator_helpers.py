53e8be51342ff1cd7afc87f948ce02cc89ac5360
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-09-02 20:41:05 +0200

    test: create tests for resolve_type helper function

--- tests/test_query_creator_helpers.py
+++ tests/test_query_creator_helpers.py
@@ -1,5 +1,8 @@
+import enum
+import typing
 import gqlrequests
 from gqlrequests.query_creator import generate_fields, generate_query_string, generate_function_query_string
+from gqlrequests.query_creator import FieldTypeEnum, resolve_type
 
 
 def test_generate_fields():
@@ -66,3 +69,35 @@ getSomething(id: 1, number: 3.14, name: \"John\", isCool: true) {
     }
 
     assert generate_function_query_string("getSomething", args, fields) == correct_string
+
+def test_resolve_type_primitive():
+    assert resolve_type(int) == (FieldTypeEnum.PRIMITIVE, int)
+    assert resolve_type(float) == (FieldTypeEnum.PRIMITIVE, float)
+    assert resolve_type(str) == (FieldTypeEnum.PRIMITIVE, str)
+    assert resolve_type(bool) == (FieldTypeEnum.PRIMITIVE, bool)
+
+def test_resolve_type_enum():
+    class TestEnum(enum.Enum):
+        VALUE = 1
+
+    assert resolve_type(TestEnum) == (FieldTypeEnum.ENUM, TestEnum)
+
+def test_resolve_type_query_builder_class():
+    class TestQueryBuilder(gqlrequests.QueryBuilder):
+        name: str
+
+    assert resolve_type(TestQueryBuilder) == (FieldTypeEnum.QUERY_BUILDER_CLASS, TestQueryBuilder)
+
+def test_resolve_type_query_builder_instance():
+    class TestQueryBuilder(gqlrequests.QueryBuilder):
+        name: str
+
+    instance = TestQueryBuilder()
+    assert resolve_type(instance) == (FieldTypeEnum.QUERY_BUILDER_INSTANCE, instance)
+
+def test_resolve_list_field():
+    class Test:
+        age: list[list[list[int]]]
+    
+    hints = typing.get_type_hints(Test)
+    assert resolve_type(hints["age"]) == (FieldTypeEnum.PRIMITIVE, int)
