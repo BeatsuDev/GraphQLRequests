3bdc2a4ce933d4a55513805bb9b531aea7c3bbba
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-09-10 16:16:00 +0200

    test: un-skip invalid type throws value error test

--- tests/test_creating_builder.py
+++ tests/test_creating_builder.py
@@ -82,11 +82,6 @@ class SomeClass(gqlrequests.QueryBuilder):
 class InvalidType(gqlrequests.QueryBuilder):
     invalidProperty: SomeClass
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_invalid_type_throws_value_error():
     with pytest.raises(ValueError) as e:
-        InvalidType()
-
-    # Ensure the error message contains the name of the invalid property 
-    # and the name of the class - this is to help the user debug the issue
-    assert "invalidProperty" in str(e.value) and "InvalidType" in str(e.value)
+        InvalidType().build()
