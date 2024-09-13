e6c68376650b5d723a4523a932d4e9ac2de5cfa2
BeatsuDev <ericbieszczad@gmail.com>
2024-09-13 23:58:42 +0200

    fix: typing for python 3.8

--- gqlrequests/pydantic_converter.py
+++ gqlrequests/pydantic_converter.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
 
+from typing import Type
 from .builder import QueryBuilder
 
 
-def from_pydantic(model: type[BaseModel]):
+def from_pydantic(model: Type[BaseModel]):
     return type(model.__name__, (QueryBuilder,), {"__annotations__": model.__annotations__})
