bba453a55de4c3146d329b04ab39fcd9d341c4af
BeatsuDev <ericbieszczad@gmail.com>
2024-09-14 00:11:33 +0200

    test: add more tests for converting pydantic models

--- tests/test_pydantic_models.py
+++ tests/test_pydantic_models.py
@@ -1,4 +1,6 @@
 import gqlrequests
+
+from typing import List
 from pydantic import BaseModel
 
 class EveryTypeModel(BaseModel):
@@ -20,3 +22,35 @@ def test_create_everytype():
 """[1:]
     EveryType = gqlrequests.from_pydantic(EveryTypeModel)
     assert EveryType().build() == correct_string
+
+
+class NestedTypeModel(BaseModel):
+    nested: EveryTypeModel
+
+def test_create_nestedtype():
+    correct_string = """
+{
+    nested {
+        id
+        age
+        money
+        name
+        company
+    }
+}
+"""[1:]
+    NestedType = gqlrequests.from_pydantic(NestedTypeModel)
+    assert NestedType().build() == correct_string
+
+
+class ListTypeModel(BaseModel):
+    nested: List[int]
+
+def test_create_listtype():
+    correct_string = """
+{
+    nested
+}
+"""[1:]
+    ListType = gqlrequests.from_pydantic(ListTypeModel)
+    assert ListType().build() == correct_string
