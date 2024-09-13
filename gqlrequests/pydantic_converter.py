5f4cba04769ebaa65fc07269b368fa700e59ba08
BeatsuDev <ericbieszczad@gmail.com>
2024-09-13 23:59:20 +0200

    chore: add return type hint for from_pydantic function

--- gqlrequests/pydantic_converter.py
+++ gqlrequests/pydantic_converter.py
@@ -4,5 +4,5 @@ from typing import Type
 from .builder import QueryBuilder
 
 
-def from_pydantic(model: Type[BaseModel]):
+def from_pydantic(model: Type[BaseModel]) -> Type[QueryBuilder]:
     return type(model.__name__, (QueryBuilder,), {"__annotations__": model.__annotations__})
