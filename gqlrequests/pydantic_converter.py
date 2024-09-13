f070a2449fbd6fc43f61deff8d0bd4638f9e4471
BeatsuDev <ericbieszczad@gmail.com>
2024-09-13 23:41:16 +0200

    fix: type model argument as a type, not an instance

--- gqlrequests/pydantic_converter.py
+++ gqlrequests/pydantic_converter.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 from .builder import QueryBuilder
 
 
-def from_pydantic(model: BaseModel):
+def from_pydantic(model: type[BaseModel]):
     return type(model.__name__, (QueryBuilder,), {"__annotations__": model.__annotations__})
