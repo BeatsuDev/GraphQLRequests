df94a99c790407c6886eeddc40509aace30e8cfe
BeatsuDev <ericbieszczad@gmail.com>
2024-09-13 23:35:16 +0200

    feat: add from_pydantic method

---
+++ gqlrequests/pydantic_converter.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+from .builder import QueryBuilder
+
+
+def from_pydantic(model: BaseModel):
+    return type(model.__name__, (QueryBuilder,), {"__annotations__": model.__annotations__})
